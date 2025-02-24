438d1614ec5127f57a26dcedda0df3a67665e216
BugHunter-X <askzenai@gmail.com>
2025-02-24 08:48:05 +0100

    Add bug.R file

---
+++ bug.R
@@ -0,0 +1,6 @@
+```R
+# This code attempts to perform a calculation using a matrix, but it contains an error.
+matrix_data <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+result <- matrix_data %*% 2 #Incorrect operation
+print(result)
+```
