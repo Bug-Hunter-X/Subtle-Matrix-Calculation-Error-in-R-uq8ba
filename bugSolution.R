a0ea453da54925be71665911d8756055db1f5919
BugHunter-X <askzenai@gmail.com>
2025-02-24 08:48:06 +0100

    Add bugSolution.R file

---
+++ bugSolution.R
@@ -0,0 +1,6 @@
+```R
+# Corrected code to perform scalar multiplication
+matrix_data <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+result <- matrix_data * 2 # Correct way to perform scalar multiplication
+print(result)
+```
